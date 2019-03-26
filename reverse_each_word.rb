024cd5cd53d36c0e6808f5620e17272c64122db0
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-26 21:24:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  reverse_array = array.collect |word|
-  
+  reverse_array = array.collect do |word|
+    word.
+  end
   reverse_array.join(" ")
 end
